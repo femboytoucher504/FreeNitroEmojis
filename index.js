c96d1b2f598c757172336086b0f820a891430f81
femboytoucher504 <ahmemuhsins3169@gmail.com>
2026-06-14 02:43:39 +0300

    convert to revenge compatible plugin format

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-module.exports = () => ({
-  onLoad() {
-    console.log("FreeNitroEmojis loaded!");
+/**
+ * FreeNitroEmojis - Revenge Plugin
+ */
+
+module.exports = {
+  name: "FreeNitroEmojis",
+  description: "Emoji formatting plugin for Revenge",
+  version: "1.0.0",
+
+  start() {
+    console.log("[FreeNitroEmojis] Plugin started");
+  },
+
+  stop() {
+    console.log("[FreeNitroEmojis] Plugin stopped");
   }
-});
+};
